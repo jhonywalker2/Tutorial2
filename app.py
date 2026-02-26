ff51b8bce3dda1f8f937d6c496ff2f688a299ca6
jhonywalker2 <168585042+jhonywalker2@users.noreply.github.com>
2026-02-26 10:03:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import streamlit as st 
+from PIL import Image
 
-st.title("Esta es primera app en la nube")
+st.title("Esta es mi primera app en la nube")
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales")
+
+st.write("Yo, wasgood")
+image=Image.open("Gus.jpg")
+st.image(image, caption="interfaces multimodales")
